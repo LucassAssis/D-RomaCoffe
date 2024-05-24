d557d13a82d315518ddb475b3728ae81087a3f42
LucassAssis <lucassassis@live.com>
2024-05-24 14:17:27 -0300

    Update Design

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var title = document.getElementById('title');
-    var down = document.getElementById('down');
+    var title = document.getElementById("title");
+    var down = document.getElementById("down");
 
     if (title && down) {
         var elements = [title, down];
 
         elements.forEach(function (element) {
-            element.addEventListener('mouseenter', function () {
+            element.addEventListener("mouseenter", function () {
                 elements.forEach(function (el) {
-                    el.style.transition = 'transform 1s';
-                    el.style.transform = 'translateY(10px)';
+                    el.style.transition = "transform 1s";
+                    el.style.transform = "translateY(10px)";
                 });
             });
 
-            element.addEventListener('mouseleave', function () {
+            element.addEventListener("mouseleave", function () {
                 elements.forEach(function (el) {
-                    el.style.transition = 'transform 1s';
-                    el.style.transform = 'translateY(0px)';
+                    el.style.transition = "transform 1s";
+                    el.style.transform = "translateY(0px)";
                 });
             });
         });
 
         ;
     }
-    document.getElementById("history-li").addEventListener('click', function () {
-        document.getElementById("history").scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("history-li").addEventListener("click", function () {
+        document.getElementById("history").scrollIntoView({ behavior: "smooth" });
     })
-    document.getElementById("prod-li").addEventListener('click', function () {
-        document.getElementById("sec-prod").scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("prod-li").addEventListener("click", function () {
+        document.getElementById("sec-prod").scrollIntoView({ behavior: "smooth" });
     })
 });
 
